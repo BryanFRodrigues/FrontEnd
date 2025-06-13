32d87b72e52e91fc9befdced0d01b975d7348df7
Bryan F Rodrigues <93285267+BryanFRodrigues@users.noreply.github.com>
2025-06-13 18:25:59 +0000

    Feat: Adicionado menu, banner, footer, e a main

--- frontend/src/app/pages/home-component/home-component.ts
+++ frontend/src/app/pages/home-component/home-component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "../../shared/banner-component/banner-component";
+import {FooterComponent} from "../../shared/footer-component/footer-component";
+
 
 @Component({
   selector: 'app-home-component',
-  imports: [BannerComponent],
+  imports: [BannerComponent,FooterComponent],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss'
 })
